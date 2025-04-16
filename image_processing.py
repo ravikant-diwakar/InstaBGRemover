60f5346f7a654d2f4ef366b3c1ed565110c3cdcf
ravikant-diwakar <diwakarr9562gmail.com>
2025-04-16 11:36:19 +0530

    Initial commit

--- image_processing.py
+++ image_processing.py
@@ -7,7 +7,7 @@ def remove_bg_ui():
     if uploaded_file:
         image = Image.open(uploaded_file).convert("RGB")
         output = remove(image)
-        st.image(output, caption="Image without background", use_column_width=True)
+        st.image(output, caption="Image without background", use_container_width=True)
         st.download_button("Download", data=output.tobytes(), file_name="no_bg.png")
 
 def edit_image_ui():
@@ -15,7 +15,7 @@ def edit_image_ui():
     if uploaded_file:
         image = Image.open(uploaded_file)
 
-        st.image(image, caption="Original", use_column_width=True)
+        st.image(image, caption="Original", use_container_width=True)
 
         option = st.selectbox("Edit Option", ["Crop", "Rotate", "Flip", "Resize"])
 
@@ -26,13 +26,13 @@ def edit_image_ui():
             bottom = st.number_input("Bottom", top, image.height)
             if st.button("Apply Crop"):
                 cropped = image.crop((left, top, right, bottom))
-                st.image(cropped, caption="Cropped Image", use_column_width=True)
+                st.image(cropped, caption="Cropped Image", use_container_width=True)
 
         elif option == "Rotate":
             angle = st.slider("Rotate Angle", -180, 180)
             if st.button("Rotate"):
                 rotated = image.rotate(angle)
-                st.image(rotated, caption="Rotated Image", use_column_width=True)
+                st.image(rotated, caption="Rotated Image", use_container_width=True)
 
         elif option == "Flip":
             flip = st.radio("Flip Mode", ["Horizontal", "Vertical"])
@@ -41,11 +41,11 @@ def edit_image_ui():
                     flipped = ImageOps.mirror(image)
                 else:
                     flipped = ImageOps.flip(image)
-                st.image(flipped, caption="Flipped Image", use_column_width=True)
+                st.image(flipped, caption="Flipped Image", use_container_width=True)
 
         elif option == "Resize":
             width = st.number_input("Width", 1, 1000, value=image.width)
             height = st.number_input("Height", 1, 1000, value=image.height)
             if st.button("Resize"):
                 resized = image.resize((int(width), int(height)))
-                st.image(resized, caption="Resized Image", use_column_width=True)
+                st.image(resized, caption="Resized Image", use_container_width=True)
